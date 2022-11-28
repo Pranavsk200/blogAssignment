e3e25a0921988f5a0b4da7dfd49f0cf68c717ef7
Pranavsk200 <pranavsk000@gmail.com>
2022-11-28 22:54:54 +0530

    second-commit

--- newsapp/settings.py
+++ newsapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xy_&^659mfyljh_me4o(bvx!88ht*_5^d4=a5r-)4!gr@gt_@e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- newsapp/urls.py
+++ newsapp/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+#from django.conf.url import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
